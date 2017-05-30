5a28049bc85b9e756701769cfd5b7b4fd58d1aec
petya <szrk.peter@gmail.com>
2017-05-30 11:00:13 +0200

    hello

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+/**
+ * Created by petya on 2017.05.30..
+ */
+public class Main {
+
+    public static String hello(){
+        return "hello";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(hello());
+    }
+}
